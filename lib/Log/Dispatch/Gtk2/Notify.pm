2bb12a5e13f6741cd3af2e05c8518e10c751eab0
Florian Ragwitz <rafl@debian.org>
2009-01-07 12:23:00 +0100

    Initial version.

---
+++ lib/Log/Dispatch/Gtk2/Notify.pm
@@ -0,0 +1,128 @@
+package Log::Dispatch::Gtk2::Notify;
+# ABSTRACT: send log messages to a desktop notification daemon
+
+use Moose;
+use MooseX::Types::Moose qw/Str HashRef ArrayRef CodeRef/;
+use Log::Dispatch::Gtk2::Notify::Types qw/
+    LogLevel
+    Widget
+    StatusIcon
+    Pixbuf
+    is_Widget
+    is_StatusIcon
+/;
+use File::Basename;
+use Gtk2 -init;
+use Gtk2::Notify;
+
+use namespace::clean -except => 'meta';
+
+extends qw/Moose::Object Log::Dispatch::Output/;
+
+has title => (
+    is      => 'ro',
+    isa     => Str,
+    default => sub { basename($0) },
+);
+
+has app_name => (
+    is       => 'ro',
+    isa      => Str,
+    default  => __PACKAGE__,
+);
+
+has attach_to => (
+    is  => 'ro',
+    isa => Widget|StatusIcon,
+    predicate => 'has_attach_to',
+);
+
+has icon_map => (
+    is         => 'ro',
+    isa        => HashRef[Pixbuf],
+    lazy_build => 1,
+);
+
+has name => (
+    isa      => Str,
+    required => 1,
+);
+
+has min_level => (
+    isa      => LogLevel,
+    required => 1,
+);
+
+has max_level => (
+    isa => LogLevel,
+);
+
+has callbacks => (
+    isa => (ArrayRef[CodeRef]) | CodeRef,
+);
+
+around new => sub {
+    my $orig = shift;
+    my $class = shift;
+
+    my $self = $class->$orig(@_);
+    $self->_basic_init(@_);
+
+    return $self;
+};
+
+sub BUILD {
+    my ($self) = @_;
+    Gtk2::Notify->init($self->app_name);
+}
+
+sub log_message {
+    my ($self, %args) = @_;
+
+    my $notification = Gtk2::Notify->new(
+        $self->title, $args{message},
+    );
+
+    $self->_attach_notification($notification)
+        if $self->has_attach_to;
+
+    if (my $icon = $self->icon_map->{ $args{level} }) {
+        $notification->set_icon_from_pixbuf($icon);
+    }
+
+    $notification->show;
+
+    return;
+}
+
+sub _attach_notification {
+    my ($self, $notification) = @_;
+    my $to = $self->attach_to;
+
+    if (is_StatusIcon($to)) {
+        $notification->attach_to_status_icon($to);
+    }
+    elsif (is_Widget($to)) {
+        $notification->attach_to_widget($to);
+    }
+    else {
+        confess 'FAIL';
+    }
+}
+
+sub _build_icon_map {
+    my ($self) = @_;
+    my $widget = Gtk2::Button->new;
+    return {
+        map {
+            my $pixbuf = $widget->render_icon('gtk-dialog-' . $_->[0], 'dialog');
+            (map { $_ => $pixbuf } @{ $_->[1] })
+        } (['error'   => [qw/emergency alert critical error/]],
+           ['warning' => [qw/warning/]],
+           ['info'    => [qw/notice info debug/]]),
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ lib/Log/Dispatch/Gtk2/Notify/Types.pm
@@ -0,0 +1,24 @@
+package Log::Dispatch::Gtk2::Notify::Types;
+
+use MooseX::Types::Moose qw/Str/;
+use Log::Dispatch;
+
+use namespace::clean -except => 'meta';
+
+use MooseX::Types -declare => [qw/
+    LogLevel
+    Widget
+    StatusIcon
+    Pixbuf
+/];
+
+subtype LogLevel,
+    from Str,
+    where { Log::Dispatch->level_is_valid($_) },
+    message { 'invalid log level' };
+
+class_type Widget,     { class => 'Gtk2::Widget'      };
+class_type StatusIcon, { class => 'Gtk2::StatusIcon'  };
+class_type Pixbuf,     { class => 'Gtk2::Gdk::Pixbuf' };
+
+1;
